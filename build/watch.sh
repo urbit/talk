ce71394d0ea32dd550325aa4968b99b7bd334745
Galen Wolfe-Pauly <root@office-for.com>
2016-01-28 15:53:51 -0800

    tree -> talk in build

--- build/watch.sh
+++ build/watch.sh
@@ -1,4 +1,4 @@
 #! /bin/echo "use 'npm run watch' to run this script"
 mkdir -p desk/web/tree/; 
-watchify -v -o desk/web/tree/main.js js/main.coffee & 
-node-sass -w -o desk/web/tree/ css/main.scss main.css
+watchify -v -o desk/web/talk/main.js js/main.coffee & 
+node-sass -w -o desk/web/talk/ css/main.scss main.css
