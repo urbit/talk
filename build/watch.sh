014622dd7ad0a8594a7a9704b35ebd5a0847f82a
Galen Wolfe-Pauly <root@office-for.com>
2016-01-28 16:49:09 -0800

    oops path

--- build/watch.sh
+++ build/watch.sh
@@ -1,4 +1,4 @@
 #! /bin/echo "use 'npm run watch' to run this script"
-mkdir -p desk/web/tree/; 
+mkdir -p desk/web/talk/; 
 watchify -v -o desk/web/talk/main.js js/main.coffee & 
 node-sass -w -o desk/web/talk/ css/main.scss main.css
